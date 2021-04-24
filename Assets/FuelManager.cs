0f378a17e0de1cbcf73342a26e104a849200f5fc
Ben Mora <blackopsben@gmail.com>
2021-04-24 18:17:41 -0500

    fuel upgrade working

--- Assets/FuelManager.cs
+++ Assets/FuelManager.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class FuelManager : MonoBehaviour
 {
     [SerializeField] float startingCapacity = 10f;
-    [SerializeField] float burnRate = 1f;
+    [SerializeField] float baseBurnRate = 1f;
+    [SerializeField] float burnRateIncrement = 0.1f;
+    [SerializeField] float baseEfficiencyMultiplier = 1.0f;
+    [SerializeField] float efficiencyIncrement = 0.5f;
 
     private CalculateDistance calculateDistance;
 
     private float currentCapacity;
     private float currentFuel;
 
-    private ShipManager shipManager;
+    private UpgradeManager upgradeManager;
+
+    //private ShipManager shipManager;
 
     private void Start()
     {
         calculateDistance = GetComponent<CalculateDistance>();
         currentCapacity = startingCapacity;
         currentFuel = currentCapacity;
-        shipManager = GetComponent<ShipManager>();
+        //shipManager = GetComponent<ShipManager>();
+        upgradeManager = GetComponent<UpgradeManager>();
     }
 
     private void Update()
@@ -32,8 +38,10 @@ public class FuelManager : MonoBehaviour
     {
         if (currentFuel > 0.0f)
         {
-            calculateDistance.Accelerate(Time.deltaTime * burnRate);
-            currentFuel -= Time.deltaTime * burnRate;
+            float actualBurnRate = baseBurnRate + (burnRateIncrement * (upgradeManager.GetUpgradeMultiplier(2) - 1));
+            float actualEfficiencyMultiplier = baseEfficiencyMultiplier + (efficiencyIncrement * (upgradeManager.GetUpgradeMultiplier(2) - 1));
+            calculateDistance.Accelerate(Time.deltaTime * actualBurnRate * actualEfficiencyMultiplier);
+            currentFuel -= Time.deltaTime * actualBurnRate;
             if (currentFuel < 0.0f)
             {
                 currentFuel = 0.0f;
