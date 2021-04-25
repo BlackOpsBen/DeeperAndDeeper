158372d36ba7e6bcdb58858eba61620fefe09b52
Ben Mora <blackopsben@gmail.com>
2021-04-25 16:34:13 -0500

    laser sound added

--- Assets/FireAtTarget.cs
+++ Assets/FireAtTarget.cs
@@ -8,13 +8,21 @@ public class FireAtTarget : MonoBehaviour
 
     [SerializeField] private ParticleSystem ps;
 
+    private float interval = 1f;
+
+    private float timer = 0f;
+
     private void Update()
     {
+        timer -= Time.deltaTime;
+
         if (findTarget.hasValidTarget)
         {
-            if (!ps.isPlaying)
+            if (timer < 0.0f)
             {
                 ps.Play();
+                AudioManager.Instance.PlaySFX("Laser");
+                timer = interval;
             }
         }
         else
