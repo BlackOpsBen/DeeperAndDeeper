3d64d8ea61847563ee38f4a160348ffa419cd774
Ben Mora <blackopsben@gmail.com>
2021-04-25 13:45:57 -0500

    Fixed UI bug

--- Assets/UpgradeManager.cs
+++ Assets/UpgradeManager.cs
@@ -6,7 +6,7 @@ public class UpgradeManager : MonoBehaviour
 {
     [SerializeField] private UpgradeOption[] upgradeOptions;
 
-    private void Start()
+    private void Awake()
     {
         foreach (UpgradeOption upgradeOption in upgradeOptions)
         {
